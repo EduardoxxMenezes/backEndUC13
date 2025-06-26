5aaade360e8e5acdc21df42ec4851e4a90355cbf
Eduardo Menezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2025-06-26 14:17:54 -0300

    Update UserRepositories.ts

--- trabalhoAvaliativoBack/src/repository/UserRepositories.ts
+++ trabalhoAvaliativoBack/src/repository/UserRepositories.ts
@@ -6,27 +6,27 @@ export class UserRepository{
     private UserRep = AppDataSource.getRepository(User);
 
 
-    async createUser(nome: string, email: string, senha: string, userType: string){
+    async createUser(nome: string, email: string, senha: string, userType: string){ //cria usuario 
 
         const UserCreate = new User(nome,email, senha, userType);
         
-        return await this.UserRep.save(UserCreate);
+        return await this.UserRep.save(UserCreate); //cria o novo usuario no banco de dados.
     }
 
-    async findUserByEmail(email: string){
+    async findUserByEmail(email: string){ //encontra o usuario pelo email
 
         return await this.UserRep.findOneBy({ email: email});
     }
 
-    async findUserByName(nome: string){
+    async findUserByName(nome: string){ //encontra usuario pelo nome
         return await this.UserRep.findOneBy({ name: nome});
     }
 
-    async findUserById(id: number ) {
+    async findUserById(id: number ) { //encontra usuario pelo ID
         return await this.UserRep.findOneBy({ id: id});
     }
 
-    async updateUser(id: number,  fields: Partial<User>){
+    async updateUser(id: number,  fields: Partial<User>){ //atualia usuario.
 
         const user = await this.findUserById(id);
         if (user) {
@@ -37,13 +37,13 @@ export class UserRepository{
         return null;
     }
 
-    async deleteUser(id: number){
+    async deleteUser(id: number){ //deleta usuario
         const userCreated = await this.findUserById(id);
         if (!userCreated) return null;
         return await this.UserRep.remove(userCreated);
     }
 
-    async findAllUsers() {
+    async findAllUsers() { //seleciona todos os usuarios no banco de dados
         return await this.UserRep.find();
       }
 }
